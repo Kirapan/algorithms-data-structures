dd9ce5d4889b9aa644b62f65859ff7c1214ff6a5
晓奇潘 <xiaoqipan@hans-MacBook-Pro.local>
2019-05-12 21:43:06 -0400

    Day 3

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,13 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+    for (let char of str) {
+        charMap[char] = charMap[char]++ || 1;
+    }
+    
+    return Object.keys(charMap).reduce((acc, key) => charMap[acc] < charMap[key] ? key : acc);
+}
 
 module.exports = maxChar;
