f2f3e3f657b53707bbd56c93734cc20498196040
晓奇潘 <xiaoqipan@hans-MacBook-Pro.local>
2019-05-26 19:05:58 -0400

    queue and stack

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,47 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//     let a = 0;
+//     let b = 1;
+//     let c;
+//     if (n < 2) c = n;
+//     for(let i = 2; i <= n; i++) {
+//         c = a + b;
+//         a = b;
+//         b = c;
+//     }
+//     return c;
+// }
+
+// function fib(n) {
+// 	let result;
+//   if(n < 2) {
+// 		return n;
+// 	}
+// 	return fib(n-2) + fib(n-1);
+	
+// }
+
+function memorize(fn) {
+	const cache = {};
+	return function(...args) {
+		if(cache[args]) {
+			return cache[args]
+		}
+		const result = fn.apply(this, args)
+		cache[args] = result;
+		return result;
+	}
+}
+
+function slowFib(n) {
+	let result;
+  if(n < 2) {
+		return n;
+	}
+	return fib(n-2) + fib(n-1);
+}
+const fib = memorize(slowFib);
 
 module.exports = fib;
