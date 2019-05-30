4dd9a31a929d0e159df397449c0775c003bb1072
晓奇潘 <xiaoqipan@hans-MacBook-Pro.local>
2019-05-29 22:19:26 -0400

    more on linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,63 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+	constructor(data, next = null) {
+		this.data = data;
+		this.next = next;
+	}
+}
 
-class LinkedList {}
+class LinkedList {
+	constructor() {
+		this.head = null
+	}
+	insertFirst(data) {
+		this.head = new Node(data, this.head);
+	}
+	size() {
+		let next = this.head;
+		let count = 0;
+		while (next) {
+			next = next.next;
+			count++;
+		}
+		return count;
+	}
+	getFirst() {
+		return this.head;
+	}
+	getLast() {
+		if(!this.head) return null;
+		let node = this.head;
+		while (node.next) {
+			node = node.next;
+		}
+		return node;
+	}
+	clear() {
+		this.head = null;
+	}
+	removeFirst() {
+		if (!this.head) return;
+		this.head = this.head.next;
+	}
+	removeLast() {
+		if(!this.head) return;
+		if(!this.head.next) {
+			this.head = null;
+			return;
+		}
+		let previous = this.head;
+		let node = this.head.next;
+		while (node.next) {
+			previous = node;
+			node = node.next;
+		}
+		previous.next = null;
+	}
+}
+
+
 
 module.exports = { Node, LinkedList };
