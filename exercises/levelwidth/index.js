344c58adde0ae7aa678c22e6b1218abce3ef95a7
晓奇潘 <xiaoqipan@hans-MacBook-Pro.local>
2019-06-15 15:24:12 -0400

    all done

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,20 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let arr = [root, 'end'];
+    let counters = [0];
+    while (arr.length > 1) {
+        const node = arr.shift();
+        if(node !== 'end') {
+            counters[counters.length - 1]++;
+            arr.push(...node.children)
+        } else {
+            counters.push(0);
+            arr.push('end');
+        }
+    }
+    return counters;
+}
 
 module.exports = levelWidth;
